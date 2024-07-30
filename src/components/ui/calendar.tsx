adf0ee498a2fcffe7adbb7a460add7121f651a40
SourishBiswas23 <sourish135@gmail.com>
2024-07-30 17:22:32 +0530

    Fixed the build errors

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,8 +26,8 @@ function Calendar({
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
-          buttonVariants({ variant: "green" }),
-          "h-7 w-7 bg-green text-white p-0 opacity-50 hover:opacity-90"
+          buttonVariants({ variant: "default" }),
+          "h-7 w-7 bg-green text-white p-0 opacity-50 hover:opacity-90",
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
@@ -37,8 +37,8 @@ function Calendar({
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-slate-100/50 [&:has([aria-selected])]:bg-slate-100 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50 dark:[&:has([aria-selected])]:bg-slate-800",
         day: cn(
-          buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+          buttonVariants({ variant: "default" }),
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100",
         ),
         day_range_end: "day-range-end",
         day_selected:
